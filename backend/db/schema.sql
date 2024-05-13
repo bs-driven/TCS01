812f6f123e862ec508ae362f7cbf264d33554387
Bradley <bradsamsess90@icloud.com>
2024-05-13 13:54:15 -0400

    start of seed info

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -4,10 +4,11 @@ CREATE DATABASE therapistClient_db;
 USE therapistClient_db;
 
 CREATE TABLE therapists(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    therapist_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     email_addy VARCHAR(50) NOT NULL,
+    admin_priv BOOLEAN NOT NULL,
     
 );
 
@@ -18,17 +19,24 @@ CREATE TABLE clients (
     therapist_id INT NOT NULL,
     email_address VARCHAR(50) NOT NULL,
     FOREIGN KEY (therapist_id)
-    REFERENCES therapists(id)
+    REFERENCES therapists(therapist_id)
 );
- CREATE TABLE tc_schedules(
-    tc_schedule id NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tc_date DATE,
-    tc_time TIME,
+ CREATE TABLE appointments(
+    appointment_id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    appointment_note TEXT(300)
+    appointment_time TIME,
+    therapist_id INT,
+    client_id INT,
+
  );
  CREATE TABLE dapNotes(
     dap_id NOT NULL AUTO_INCREMENT PRIMARY KEY,
     dap_note TEXT(6000),
-    dap_date DATE,
+    dap_date_time DATETIME,
+    therapist_id INT NOT NULL,
+    client_id   INT NOT NULL,
+    dap_signature BOOLEAN,
+
  );
 
  CREATE TABLE soapNotes(
@@ -47,5 +55,6 @@ CREATE TABLE clients (
     message_id NOT NULL AUTO_INCREMENT PRIMARY KEY,
     to_someone TEXT,
     from_someone TEXT,
+    messageDate DATE,
  );
 
---
+++ backend/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO therapists(id,first_name, last_name, email_addy, admin_priv)
+(0001,"luffy","king", "priate_king@email.com", true)
+(0002, "gon", "freaks", "truehunter@hunterhunter.com", false)
